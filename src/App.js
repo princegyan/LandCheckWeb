da63bccf5cf3344c1eaea0c3eb6475f8e79874b9
princegyan <princealfredgyan@gmail.com>
2022-06-15 21:59:41 +0000

    Service and Login Modal

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
     <div className="  bg-cover bg-center lg:w-full h-full " style={{ backgroundImage: "url('https://media.istockphoto.com/photos/landscape-in-west-africa-picture-id519804447?k=6&m=519804447&s=612x612&w=0&h=FmxTlS9UAPb-QILrKtLHKX3vlZT_oT04uYlxP7C1vvE=')" }}>
       
       <Sidebar/>
-      {/* <ModalSignIn/>  */}
       
           <>
       <h5 className=" pt-40 text-4xl font-bold text-[#060646] dark:text-white text-center">
