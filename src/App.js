b622981d05dec65f2787adcdd93a378d9f9d285f
princegyan <princealfredgyan@gmail.com>
2022-06-16 00:59:44 +0000

    Complete HomePage Setup

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <div className="  bg-cover bg-center lg:w-full h-full " style={{ backgroundImage: "url('https://media.istockphoto.com/photos/landscape-in-west-africa-picture-id519804447?k=6&m=519804447&s=612x612&w=0&h=FmxTlS9UAPb-QILrKtLHKX3vlZT_oT04uYlxP7C1vvE=')" }}>
       
       <Sidebar/>
+      <img className=' fixed left-10 top-6' src={'Logo.png'} alt={'LandCheck Logo'}/>
       
           <>
       <h5 className=" pt-40 text-4xl font-bold text-[#060646] dark:text-white text-center">
